358a130e3c6ac27eb82d4609324dc89fa9a9ffd5
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-07-07 22:53:32 +0300

    added terrain atlas and config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import './index.scss';
 import SenseiWalk from './assets/Male-4-Walk.png';
+import terrainAtlas from './assets/terrain.png';
+import worldConfig from './configs/world.json';
+import sprites from './configs/sprites';
 
 const canvas = document.getElementById('game');
-const map = document.getElementById('map');
+const mapCanvas = document.getElementById('map');
 const context = canvas.getContext('2d');
-const mapContext = map.getContext('2d');
+const mapContext = mapCanvas.getContext('2d');
 
 const image = document.createElement('img');
 image.src = SenseiWalk;
 
-const spriteWidth = 48;
-const spriteHeigth = 48;
+const terrain = document.createElement('img');
+terrain.src = terrainAtlas;
+
+const spriteW = 48;
+const spriteH = 48;
 const shots = 3;
 let cycle = 1;
-let pY = (canvas.offsetHeight - spriteHeigth) / 2; // Canvas Position on Y axis
-let pX = (canvas.offsetWidth - spriteWidth) / 2; // Canvas Position on X axis
+let pY = (canvas.offsetHeight - spriteH) / 2; // Canvas Position on Y axis
+let pX = (canvas.offsetWidth - spriteW) / 2; // Canvas Position on X axis
 let direction = '';
 let side = 0;
 
@@ -24,22 +30,22 @@ function keyDownHandler(event) {
   if (event.key === 'Down' || event.key === 'ArrowDown') {
     bottomPressed = true;
     direction = 'down';
-    side = spriteHeigth * 0;
+    side = spriteH * 0;
   }
   if (event.key === 'Left' || event.key === 'ArrowLeft') {
     bottomPressed = true;
     direction = 'left';
-    side = spriteHeigth * 1;
+    side = spriteH * 1;
   }
   if (event.key === 'Right' || event.key === 'ArrowRight') {
     bottomPressed = true;
     direction = 'right';
-    side = spriteHeigth * 2;
+    side = spriteH * 2;
   }
   if (event.key === 'Up' || event.key === 'ArrowUp') {
     bottomPressed = true;
     direction = 'up';
-    side = spriteHeigth * 3;
+    side = spriteH * 3;
   }
 }
 
@@ -67,7 +73,7 @@ image.addEventListener('load', () => {
     if (bottomPressed) {
       switch (direction) {
         case 'down':
-          if (pY <= canvas.offsetHeight - spriteHeigth) {
+          if (pY <= canvas.offsetHeight - spriteH) {
             pY += 10;
           }
           break;
@@ -77,7 +83,7 @@ image.addEventListener('load', () => {
           }
           break;
         case 'right':
-          if (pX <= canvas.offsetWidth - spriteWidth) {
+          if (pX <= canvas.offsetWidth - spriteW) {
             pX += 10;
           }
           break;
@@ -92,7 +98,7 @@ image.addEventListener('load', () => {
       cycle = (cycle + 1) % shots;
     }
     context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
-    context.drawImage(image, cycle * spriteWidth, side, spriteWidth, spriteHeigth, pX, pY, 48, 48);
+    context.drawImage(image, cycle * spriteW, side, spriteW, spriteH, pX, pY, 48, 48);
   }, 120);
 });
 
@@ -102,69 +108,80 @@ setTimeout(() => {
   document.getElementById('loading').style.opacity = 'none';
 }, 500);
 
-mapContext.fillStyle = '#c79a5b';
+// mapContext.fillStyle = '#c79a5b';
 
-mapContext.fillRect(250, 400, 100, 200);
-mapContext.fillRect(0, 400, 300, 100);
-mapContext.fillRect(300, 400, 300, 100);
-mapContext.fillRect(100, 0, 100, 400);
-mapContext.fillRect(400, 0, 100, 400);
+// mapContext.fillRect(250, 400, 100, 200);
+// mapContext.fillRect(0, 400, 300, 100);
+// mapContext.fillRect(300, 400, 300, 100);
+// mapContext.fillRect(100, 0, 100, 400);
+// mapContext.fillRect(400, 0, 100, 400);
 
-mapContext.fillStyle = '#3da5ff';
-mapContext.fillRect(200, 0, 200, 200);
+// mapContext.fillStyle = '#3da5ff';
+// mapContext.fillRect(200, 0, 200, 200);
 
-mapContext.fillStyle = '#1fb526';
+// mapContext.fillStyle = '#1fb526';
 
-mapContext.beginPath();
-mapContext.moveTo(50, 50);
-mapContext.lineTo(75, 100);
-mapContext.lineTo(25, 100);
-mapContext.fill();
-mapContext.beginPath();
-mapContext.moveTo(50, 70);
-mapContext.lineTo(75, 120);
-mapContext.lineTo(25, 120);
-mapContext.fill();
-mapContext.fillStyle = '#c79a5b';
-mapContext.fillRect(45, 120, 10, 20);
+// mapContext.beginPath();
+// mapContext.moveTo(50, 50);
+// mapContext.lineTo(75, 100);
+// mapContext.lineTo(25, 100);
+// mapContext.fill();
+// mapContext.beginPath();
+// mapContext.moveTo(50, 70);
+// mapContext.lineTo(75, 120);
+// mapContext.lineTo(25, 120);
+// mapContext.fill();
+// mapContext.fillStyle = '#c79a5b';
+// mapContext.fillRect(45, 120, 10, 20);
 
-mapContext.fillStyle = '#1fb526';
+// mapContext.fillStyle = '#1fb526';
 
-mapContext.beginPath();
-mapContext.moveTo(550, 530);
-mapContext.lineTo(575, 580);
-mapContext.lineTo(525, 580);
-mapContext.fill();
-mapContext.beginPath();
-mapContext.moveTo(550, 510);
-mapContext.lineTo(575, 560);
-mapContext.lineTo(525, 560);
-mapContext.fill();
-mapContext.fillStyle = '#c79a5b';
-mapContext.fillRect(545, 580, 10, 20);
+// mapContext.beginPath();
+// mapContext.moveTo(550, 530);
+// mapContext.lineTo(575, 580);
+// mapContext.lineTo(525, 580);
+// mapContext.fill();
+// mapContext.beginPath();
+// mapContext.moveTo(550, 510);
+// mapContext.lineTo(575, 560);
+// mapContext.lineTo(525, 560);
+// mapContext.fill();
+// mapContext.fillStyle = '#c79a5b';
+// mapContext.fillRect(545, 580, 10, 20);
 
-mapContext.fillStyle = '#1fb526';
+// mapContext.fillStyle = '#1fb526';
 
-mapContext.beginPath();
-mapContext.moveTo(550, 250);
-mapContext.lineTo(575, 300);
-mapContext.lineTo(525, 300);
-mapContext.fill();
-mapContext.beginPath();
-mapContext.moveTo(550, 220);
-mapContext.lineTo(575, 270);
-mapContext.lineTo(525, 270);
-mapContext.fill();
-mapContext.fillStyle = '#c79a5b';
-mapContext.fillRect(545, 300, 10, 20);
+// mapContext.beginPath();
+// mapContext.moveTo(550, 250);
+// mapContext.lineTo(575, 300);
+// mapContext.lineTo(525, 300);
+// mapContext.fill();
+// mapContext.beginPath();
+// mapContext.moveTo(550, 220);
+// mapContext.lineTo(575, 270);
+// mapContext.lineTo(525, 270);
+// mapContext.fill();
+// mapContext.fillStyle = '#c79a5b';
+// mapContext.fillRect(545, 300, 10, 20);
 
-mapContext.fillStyle = '#999';
-mapContext.fillRect(520, 100, 70, 90);
-mapContext.fillRect(510, 190, 90, 20);
-mapContext.fillRect(510, 80, 90, 20);
-mapContext.fillRect(510, 60, 20, 20);
-mapContext.fillRect(545, 60, 20, 20);
-mapContext.fillRect(580, 60, 20, 20);
+// mapContext.fillStyle = '#999';
+// mapContext.fillRect(520, 100, 70, 90);
+// mapContext.fillRect(510, 190, 90, 20);
+// mapContext.fillRect(510, 80, 90, 20);
+// mapContext.fillRect(510, 60, 20, 20);
+// mapContext.fillRect(545, 60, 20, 20);
+// mapContext.fillRect(580, 60, 20, 20);
 
-mapContext.fillStyle = '#8a3f06';
-mapContext.fillRect(540, 140, 30, 40);
+// mapContext.fillStyle = '#8a3f06';
+// mapContext.fillRect(540, 140, 30, 40);
+
+terrain.addEventListener('load', () => {
+  const { map } = worldConfig;
+  map.forEach((cfgRow, y) => {
+    // cfgRow -> item, y -> index
+    cfgRow.forEach((cfgCell, x) => {
+      const [sX, sY, sW, sH] = sprites.terrain[cfgCell[0]].frames[0];
+      mapContext.drawImage(terrain, sX, sY, sW, sH, x * spriteW, y * spriteH, spriteW, spriteH);
+    });
+  });
+});
